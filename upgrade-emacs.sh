cc43e7c507ff59ca60a29ef462a5b04e42d3654b
Atul Patil <atpatil@apstra.com>
2019-06-12 15:04:42 -0700

    emacs upgrade script fixed to update symbolic links

--- upgrade-emacs.sh
+++ upgrade-emacs.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+set -ex
+
 sudo add-apt-repository ppa:kelleyk/emacs
 sudo apt update
 sudo apt install -y \
 	emacs26 \
     cscope \
     clang
- 
+
+[ -f /etc/alternatives/emacs-26.2 ] && echo "/etc/alternatives/emacs-26.2 not installed properly"
+ln -fs /etc/alternatives/emacs-26.2 /etc/alteratives/emacs
+[ emacs --version | head -1 | awk '{print $NF }' | grep 26 ] && "Failed to upgrade emacs to emacs-26"
